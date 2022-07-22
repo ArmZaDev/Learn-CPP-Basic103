f4b72b748e11e6b37eccb78bb0123c864b21f09d
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-22 14:28:37 +0700

    Switch statement

--- MyProgram03/MyProgram03.cpp
+++ MyProgram03/MyProgram03.cpp
@@ -1,37 +1,55 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int temperature;
+    int month;
 
-    cout << "Please enter the temperature (C): ";
-    cin >> temperature;
+    cout << "Enter the month (1-12):";
+    cin >> month;
 
-    if (temperature >= 30) {
-        cout << "It is HOT outside ,:/";
-    }
-    else if (temperature <= 0) {
-        cout << "It is COLD outside *brrrrr*";
-    }
-    else {
-        cout << "It is warm outside :)";
-    }
-
-
-    /*
-    int age = 12;
-
-    if (age >= 18) 
+    switch (month) 
     {
-        cout << "cograts! You are an adult";
+        case 1:
+            cout << "It is January";
+            break;
+        case 2:
+            cout << "It is February";
+            break;
+        case 3:
+            cout << "It is March";
+            break;
+        case 4:
+            cout << "It is April";
+            break;
+        case 5:
+            cout << "It is May";
+            break;
+        case 6:
+            cout << "It is June";
+            break;
+        case 7:
+            cout << "It is July";
+            break;
+        case 8:
+            cout << "It is August";
+            break;
+        case 9:
+            cout << "It is September";
+            break;
+        case 10:
+            cout << "It is October";
+            break;
+        case 11:
+            cout << "It is November";
+            break;
+        case 12:
+            cout << "It is December";
+            break;
+        default:
+            cout << "You did not enter a number (1-12)";            
     }
-    else 
-    {
-        cout << "You are not an adult!";
-    }
-    cout << endl;
-    */
 
     return 0;
 }
