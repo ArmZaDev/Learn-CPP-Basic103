91b0c7b2f51901b5c11222a6304c278f9260af65
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-22 13:24:19 +0700

    Output, comments, and escape sequences

---
+++ MyProgram03/MyProgram03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Lyrics for a song that I enjoy
+    /*
+        These lyrics are actually very good
+        Omae wa mou... shinda iru!
+    */
+    cout << "\"Never gonna give you up\"\n";
+    cout << "Never gonna let you down" << endl;
+
+    return 0;
+}
