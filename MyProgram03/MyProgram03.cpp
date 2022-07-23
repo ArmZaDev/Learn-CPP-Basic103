10264f0f2aea27b4a9d2c8c1d4f981ef269b0c1f
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-23 14:21:36 +0700

    2D arrays

--- MyProgram03/MyProgram03.cpp
+++ MyProgram03/MyProgram03.cpp
@@ -5,34 +5,35 @@ using namespace std;
 
 int main()
 {
-    int number;
+    char keyboard[4][10]{
+        { '1','2','3','4','5','6','7','8','9','0'},
+        { 'Q','W','E','R','T','Y','U','I','O','P'},
+        { 'A','S','D','F','G','H','J','K','L',';'},
+        { 'Z','X','C','V','B','N','M',',','.','/'}
+    };
 
-    cout << "How many cars do you have?: ";
-    cin >> number;
-    cin.ignore();
-
-    //pointer
-    string* pCars = new string[number];
-
-    for (int i = 0; i < number; i++) {
-        cout << "Enter car #" << i + 1 << " ";
-        getline(cin, pCars[i]);
-    }
-
-    cout << "\nHere is your garage!\n";
-    cout << "------------------------\n";
-
-    for (int i = 0; i < number; i++) {
-        cout << "Parking spot #" << i + 1 << " " << pCars[i] << endl;
+    for (int row = 0; row < 4; row++) {
+        for (int column = 0; column < 10; column++) {
+            cout << keyboard[row][column] << " ";
+        }
+        cout << endl;
     }
 
 
-    /*string cars[] = { "Corvette","Tesla","Ferrari","Audi" };
 
-    for (int i = 0; i < size(cars); i++)
-    {
-        cout << cars[i] << endl;
+    /*int numbers[3][4] = { 
+                      { 0,1,2,3 },
+                      { 4,5,6,7, },
+                      { 8,9,10,11 } 
+                    };
+
+    for (int row = 0; row < 3; row++) {
+        for (int column = 0; column < 4; column++) {
+            cout << numbers[row][column] << " ";
+        }
+        cout << endl;
     }*/
+    
     
     cout << endl;
     return 0;
