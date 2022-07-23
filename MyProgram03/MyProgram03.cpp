99dfe84af945acc3060b00500910eec157d601a4
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-23 13:16:41 +0700

    Pointers

--- MyProgram03/MyProgram03.cpp
+++ MyProgram03/MyProgram03.cpp
@@ -1,55 +1,22 @@
 ﻿#include <iostream>
 #include <string>
 #include <algorithm>
-#include <stdlib.h> //srand, rand
-#include <time.h> //time
 using namespace std;
 
 int main()
 {
-    //pseudo-random numbers
+    //pointers
+    // & address-of operator
+    // * dereference operator
 
-    int number;
-    int response;
-    int score = 0;
+    string name = "ArmZa";
+    int age = 100;
 
-    srand(time(NULL));
+    string* pName = &name;
+    int* pAge = &age;
 
-    //high = 10, low = 1 => 1-10
-    number = rand() % 10 + 1; 
-
-    do {
-        cout << "Entr your guess (1-10): ";
-        cin >> response;
-
-        if (response > number) {
-            cout << "That was too high!\n";
-        }
-        else if (response < number) {
-            cout << "that was too low!\n";
-        }
-        else {
-            cout << "That was correct!\n";
-        }
-        score++;
-
-    } while (number != response);
-
-    cout << "\nNumber is: " << number;
-    cout << "\nguesses: " << score;
-
-
-
-    /*
-    srand(time(NULL));
-
-    int number = rand() % 10 + 1;
-    int number1 = rand() % 10 + 1;
-    int number2 = rand() % 10 + 1;
-
-    cout << number << " " << number1 << " " << number2 << " ";*/
-   
-
+    cout << *pName << endl;
+    cout << *pAge << endl;
     
 
     cout << endl;
